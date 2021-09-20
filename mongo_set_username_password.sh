a53aff2bab1d30ab105f86fdb1343264ae716fbc
Murat Ugur Eminoglu <murat@murat.ws>
2021-09-20 21:13:56 +0300

    add control

--- mongo_set_username_password.sh
+++ mongo_set_username_password.sh
@@ -3,9 +3,14 @@
 USERNAME="$1"
 PASSWORD="$2"
 
-while [ `mongo --eval 'db.User.find()' serverdb --host 127.0.0.1  | grep "_id"|wc -l` == "0" ]; do
+CHECK=$(mongo --eval 'db.User.find()' serverdb --host 127.0.0.1  | grep "_id"|wc -l)
+
+while [ "$CHECK" == "0" ]; do
 	sleep 5
 done
 
-mongo --eval 'db.User.deleteOne({ "email": "JamesBond" })' serverdb --host 127.0.0.1
+for i in "$CHECK"; do 
+	mongo --eval 'db.User.deleteOne({ "email": "JamesBond" })' serverdb --host 127.0.0.1
+done
+
 curl -s -H "Content-Type: application/json" -X POST -d '{"email":"'$USERNAME'","password":"'$PASSWORD'","userType":"ADMIN"}' http://127.0.0.1:5080/rest/v2/users/initial
