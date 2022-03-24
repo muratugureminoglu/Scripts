790dffd8788af7d71ac4f1aef736fd28f6b841c8
Murat Ugur Eminoglu <murat@murat.ws>
2022-03-24 22:05:53 +0300

    Add ffmpeg add logo script

---
+++ ffmpeg_add_logo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script is to keep the original file and add the logo.
+# https://www.ffmpeg.org/ffmpeg-filters.html
+#
+
+file_destination="/home/murat/beehob/add_logo"
+extension="*.mp4"
+logo="logo.png"
+top_left="overlay=x=(main_w-overlay_w)/(main_w-overlay_w):y=(main_h-overlay_h)/(main_h-overlay_h)"
+top_right="overlay=x=(main_w-overlay_w):y=(main_h-overlay_h)/(main_h-overlay_h)"
+center="overlay=x=(main_w-overlay_w)/2:y=(main_h-overlay_h)/2"
+
+for file in `find $file_destination -iname "$extension"`; do
+	if [[ "$file" == *"original"* ]]; then
+		break
+	else
+		new_file=$(basename $file .mp4)"_original.mp4"
+		cp $file $new_file
+		ffmpeg -y -i $new_file -i $logo -filter_complex $top_left $file
+	fi
+done
+
