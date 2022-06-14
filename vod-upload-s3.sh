dd8456c20c6db8838a7d1578639ecd799e893bb6
Murat Ugur Eminoglu <murat@murat.ws>
2022-06-14 12:20:38 +0300

    Add VoD to S3 upload script

---
+++ vod-upload-s3.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# 
+#  Installation Instructions
+#
+#  apt-get update && apt-get install ffmpeg -y
+#  vim [AMS-DIR]/webapps/applications(LiveApp or etc.)/WEB-INF/red5-web.properties
+#  settings.vodUploadFinishScript=/Script-DIR/vod-upload-s3.sh
+#  sudo service antmedia restart
+#
+
+# Check if AWS CLI is installed
+if [ -z `which aws` ]; then
+        rm -r aws* > /dev/null 2>&1
+        echo "Please wait. AWS Client is installing..."
+        curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null 2>&1
+        unzip awscliv2.zip > /dev/null 2>&1
+        sudo ./aws/install &
+        wait $!
+        echo "AWS Client installed."
+        rm -r aws*
+fi
+
+# Delete the uploaded VoD file from local disk
+DELETE_LOCAL_FILE="Y"
+
+AWS_ACCESS_KEY=""
+AWS_SECRET_KEY=""
+AWS_REGION=""
+AWS_BUCKET_NAME=""
+
+#AWS Configuration
+aws configure set aws_access_key_id $AWS_ACCESS_KEY
+aws configure set aws_secret_access_key $AWS_SECRET_KEY
+aws configure set region $AWS_REGION
+aws configure set output json
+
+
+tmpfile=$1
+mv $tmpfile ${tmpfile%.*}.mp4"_tmp"
+ffmpeg -i ${tmpfile%.*}.mp4"_tmp" -c copy -map 0 -movflags +faststart $tmpfile
+rm ${tmpfile%.*}.mp4"_tmp"
+
+aws s3 cp $tmpfile s3://$AWS_BUCKET_NAME/streams/ --acl public-read
+
+if [ $? != 0 ]; then
+        logger "$tmpfile failed to copy file to S3. "
+else
+        # Delete the uploaded file
+        if [ "$DELETE_LOCAL_FILE" == "Y" ]; then
+                aws s3api head-object --bucket $AWS_BUCKET_NAME --key streams/$(basename $tmpfile)
+                if [ $? == 0 ];then
+                        rm -rf $tmpfile
+                        logger "$tmpfile is deleted."
+                fi
+        fi
+fi
