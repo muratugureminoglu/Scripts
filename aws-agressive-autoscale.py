19f7c272ee4aa15f987f9995eb242106202d9b7d
Murat Ugur Eminoglu <murat@murat.ws>
2023-09-23 08:44:56 +0300

    Add comments

--- aws-agressive-autoscale.py
+++ aws-agressive-autoscale.py
@@ -1,9 +1,11 @@
 import boto3
 
+# AWS credentials and region
 aws_access_key = ''
 aws_secret_key = ''
 aws_region = ''
 
+# Constants for instance limits
 C5_XLARGE_EDGE_LIMIT = 150
 C5_4XLARGE_EDGE_LIMIT = C5_XLARGE_EDGE_LIMIT * 4
 C5_9XLARGE_EDGE_LIMIT = C5_XLARGE_EDGE_LIMIT * 7
@@ -11,32 +13,35 @@ C5_XLARGE_ORIGIN_LIMIT = 40
 C5_4XLARGE_ORIGIN_LIMIT = C5_XLARGE_ORIGIN_LIMIT * 4
 C5_9XLARGE_ORIGIN_LIMIT = C5_XLARGE_ORIGIN_LIMIT * 9
 
-
+# Initialize AWS clients
 autoscaling_client = boto3.client('autoscaling', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=aws_region)
 ec2_client = boto3.client('ec2', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=aws_region)
 
-# Find automatically ASG names
+# Find Auto Scaling Group names with specific prefixes
 asg_names = autoscaling_client.describe_auto_scaling_groups()
 asg_edge_name = [group for group in asg_names['AutoScalingGroups'] if 'EdgeGroup' in group['AutoScalingGroupName']]
 asg_origin_name = [group for group in asg_names['AutoScalingGroups'] if 'OriginGroup' in group['AutoScalingGroupName']]
 asg_edge_group_names = [group['AutoScalingGroupName'] for group in asg_edge_name][0]
 asg_origin_group_names = [group['AutoScalingGroupName'] for group in asg_origin_name][0]
 
+# Describe Auto Scaling Groups
 edge_autoscaling_group = autoscaling_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_edge_group_names])
 origin_autoscaling_group = autoscaling_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_origin_group_names])
 
+# Get instance types and current instance counts
 edge_instance_type = edge_autoscaling_group['AutoScalingGroups'][0]['Instances'][0]['InstanceType']
 origin_instance_type = edge_autoscaling_group['AutoScalingGroups'][0]['Instances'][0]['InstanceType']
-
 edge_current_instance_count = len(edge_autoscaling_group['AutoScalingGroups'][0]['Instances'])
 origin_current_instance_count = len(origin_autoscaling_group['AutoScalingGroups'][0]['Instances'])
 
 #instance_type='c5.xlarge'
 current_instance_count=int(2)
 
+# Set viewer and publisher counts
 viewer_count = 300
 publisher_count = 50
 
+# Function to check and upgrade Auto Scaling Group capacity
 def check_and_upgrade(count, current_instance_count, asg_name):
     if count > current_instance_count:
         response = autoscaling_client.update_auto_scaling_group(
@@ -45,6 +50,7 @@ def check_and_upgrade(count, current_instance_count, asg_name):
             MinSize=count
         )
 
+# Check and upgrade Auto Scaling Group if instance type matches
 if edge_instance_type == "c5.xlarge":
     edge_count = -(-viewer_count // C5_XLARGE_EDGE_LIMIT)
     print (edge_count)
